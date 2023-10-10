5d1c0bf73dc139de21fef316498d96eafd65d3f4
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2023-10-10 15:27:55 +0900

    02_02

--- Input.h
+++ Input.h
@@ -1,5 +1,16 @@
 #pragma once
 #include <windows.h>
+
+#define DIRECTINPUT_VERSION     0x0800   // DirectInputのバージョン指定
+#include <dinput.h>
+
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+#include <wrl.h>
+
+//using namespace Microsoft::WRL;
+
 class Input{
 public:	//メンバ関数
 	//初期化
@@ -7,5 +18,8 @@ public:	//メンバ関数
 	//更新
 	void Update();
 
+private:
+	// キーボードデバイスの生成
+	Microsoft::WRL::ComPtr<IDirectInputDevice8> keyboard;
 };
 
