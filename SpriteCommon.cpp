aa9984ec627300d008c86006e944a869a4f93779
Shibuyanaoki <115063031+Shibuyanaoki@users.noreply.github.com>
2024-01-23 16:45:10 +0900

    a

--- SpriteCommon.cpp
+++ SpriteCommon.cpp
@@ -12,12 +12,13 @@ void SpriteCommon::Initialize()
 	//DXCèâä˙âª
 	ComPtr<IDxcUtils> dxcUtils;
 	ComPtr<IDxcCompiler3> dxcCompiler;
+	ComPtr<IDxcIncludeHandler> includeHandler;
+
 	result = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&dxcUtils));
 	assert(SUCCEEDED(result));
 	result = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&dxcCompiler));
 	assert(SUCCEEDED(result));
 
-	ComPtr<IDxcIncludeHandler> includeHandler;
 	result = dxcUtils->CreateDefaultIncludeHandler(&includeHandler);
 	assert(SUCCEEDED(result));
 
--- SpriteCommon.h
+++ SpriteCommon.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <Windows.h>
 #include <wrl.h>
 #include <string>
 #include <dxcapi.h>
